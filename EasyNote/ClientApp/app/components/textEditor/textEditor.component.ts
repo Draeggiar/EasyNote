b24f3c6fc992bf8dedc35d3cf8e06323a249f6d7
Draeggiar <tama2@wp.pl>
2018-11-20 18:06:27 +0100

    Create clean angular app

--- EasyNote/ClientApp/app/components/textEditor/textEditor.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'text-editor',
-    templateUrl: './textEditor.component.html',
-    styleUrls: ['./textEditor.component.scss']
-})
-export class TextEditorComponent {
-}
