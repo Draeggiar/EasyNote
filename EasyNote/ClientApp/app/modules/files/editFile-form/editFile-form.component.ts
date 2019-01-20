2b6418bd6783e45ed65ad4a70b4b583050179089
Franqu <maciejedrzejko@gmail.com>
2019-01-20 21:22:42 +0100

    forced reloading of page for navbar content update when saving/creating file

--- EasyNote/ClientApp/app/modules/files/editFile-form/editFile-form.component.ts
+++ EasyNote/ClientApp/app/modules/files/editFile-form/editFile-form.component.ts
@@ -1,48 +1,57 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { finalize } from 'rxjs/operators';
-import { FileAdd } from '../model/file.add.interface';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { File } from '../model/file.interface';
 import { FilesService } from '../../../services/files.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'ClientApp/app/services/user.service';
 import { Observable } from 'rxjs';
+import { FilesListComponent } from 'ClientApp/app/components/files-list/files-list.component';
 
 @Component({
+  providers: [FilesListComponent],
   selector: 'app-editFile-form',
   templateUrl: './editFile-form.component.html',
   styleUrls: ['./editFile-form.component.scss']
 })
-export class EditFileFormComponent implements OnInit {
-
-  errors: string;
-  isRequesting: boolean;
-  submitted: boolean = false;
-
-  file: Observable<FileAdd>;
-
-  constructor(private filesService: FilesService) {
-
+export class EditFileFormComponent implements OnInit  {
+  fileId: string;
+  file: File;
+  filesList: Observable<File>;
+  constructor(private fileListComp : FilesListComponent,private filesService: FilesService, private route: ActivatedRoute,
+    private userService: UserService, private router: Router) {
   }
 
-  getFile({ value}: { value: FileAdd}) {
-    this.submitted = true;
-    this.isRequesting = true;
-    this.errors = '';
-    this.file = this.filesService.getFile( value.id);
-   
-
-
+  getFile(id: string) {
+    //TODO Spinner przy ładowaniu pliku
+    this.filesService.getFile(id).subscribe(f => this.file = f);
   }
-  saveFile({ value}: { value: FileAdd}) {
-    this.submitted = true;
-    this.isRequesting = true;
-    this.errors = '';
-    this.file = this.filesService.saveFile( value.id ,value.content);
-   
-
-
+  myEvent = new EventEmitter();
+  saveFile({ value }: { value: File }) {
+    //TODO Spinner przy zapsie
+    if (this.fileId !== '0') {
+      this.filesService.saveFile(this.fileId, value.name, value.content);
+     
+      window.location.reload();
+    }
+    else {
+      let newFileId;
+      this.filesService.createFile(value.name, this.userService.getNameOfLoggedUser(), value.content)
+        .subscribe(f => newFileId = f);
+        this.filesList = this.filesService.getFilesList();
+      this.router.navigateByUrl(this.router.url.replace("id", newFileId));
+      window.location.reload();
+    }
   }
+
   ngOnInit() {
-    
+    this.route.paramMap.subscribe(params => {
+      this.fileId = params.get("id");
+
+      if (this.fileId !== '0') {
+        this.getFile(this.fileId);
+      }
+      else {
+        this.file = <File>{ name: "", content: "" };
+      }
+    });
   }
-  
-  
 }
